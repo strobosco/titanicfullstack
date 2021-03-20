5eada3dce7956f77bcf19522787ddde816512d03
Niccolo <niccolonobili@gmail.com>
2021-03-20 17:49:21 +0100

    Added env variables

--- api/server.js
+++ api/server.js
@@ -1,14 +1,18 @@
 const express = require("express");
 const app = express();
 var cors = require('cors');
+const dotenv = require('dotenv');
+dotenv.config()
 const predict = require("./functions/predictSurvival");
 
-const PORT = process.env.PORT || 8080;
+const PORT = parseInt(process.env.PORT) || 8080;
+const POST_URL = process.env.POST_URL;
+console.log(typeof PORT)
 
 app.use(cors());
 app.use(express.json(limit="1mb"));
 
-app.post("/api", (req, res) => {
+app.post(POST_URL, (req, res) => {
 
     console.log("Request received")
 
